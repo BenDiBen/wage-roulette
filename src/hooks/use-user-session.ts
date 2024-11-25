685a3c8953397a1f433760a6323d146a1c9a58a2
Ben Diesel <befkotze@gmail.com>
2024-11-25 11:27:51 +0200

    Update use user session to arrow function

--- src/hooks/use-user-session.ts
+++ src/hooks/use-user-session.ts
@@ -4,42 +4,44 @@ import type { User } from "firebase/auth";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
-function useUserSession(initialUser: User) {
-    // The initialUser comes from the server via a server component
-    const [user, setUser] = useState<User | null>(initialUser);
-    const { refresh } = useRouter();
+export const useUserSession = (initialUser: User) => {
+	// The initialUser comes from the server via a server component
+	const [user, setUser] = useState<User | null>(initialUser);
+	const { refresh } = useRouter();
 
-    // Register the service worker that sends auth state back to server
-    // The service worker is built with npm run build-service-worker
-    useEffect(() => {
-            if ("serviceWorker" in navigator) {
-                    const serializedFirebaseConfig = encodeURIComponent(JSON.stringify(firebaseConfig));
-                    const serviceWorkerUrl = `/auth-service-worker.js?firebaseConfig=${serializedFirebaseConfig}`
-            
-              navigator.serviceWorker
-                    .register(serviceWorkerUrl)
-                    .then((registration) => console.log("scope is: ", registration.scope));
-            }
-      }, []);
+	// Register the service worker that sends auth state back to server
+	// The service worker is built with npm run build-service-worker
+	useEffect(() => {
+		if ("serviceWorker" in navigator) {
+			const serializedFirebaseConfig = encodeURIComponent(
+				JSON.stringify(firebaseConfig),
+			);
+			const serviceWorkerUrl = `/auth-service-worker.js?firebaseConfig=${serializedFirebaseConfig}`;
 
-    useEffect(() => {
-            const unsubscribe = onAuthStateChanged((authUser) => {
-                    setUser(authUser)
-            })
+			navigator.serviceWorker
+				.register(serviceWorkerUrl)
+				.then((registration) => console.log("scope is: ", registration.scope));
+		}
+	}, []);
 
-            return () => unsubscribe()
-    }, []);
+	useEffect(() => {
+		const unsubscribe = onAuthStateChanged((authUser) => {
+			setUser(authUser);
+		});
 
-    useEffect(() => {
-            onAuthStateChanged((authUser) => {
-                    if (user === undefined) return
+		return () => unsubscribe();
+	}, []);
 
-                    // refresh when user changed to ease testing
-                    if (user?.email !== authUser?.email) {
-                            refresh()
-                    }
-            })
-    }, [refresh, user])
+	useEffect(() => {
+		onAuthStateChanged((authUser) => {
+			if (user === undefined) return;
 
-    return user;
-}
+			// refresh when user changed to ease testing
+			if (user?.email !== authUser?.email) {
+				refresh();
+			}
+		});
+	}, [refresh, user]);
+
+	return user;
+};
